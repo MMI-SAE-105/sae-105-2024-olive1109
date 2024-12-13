849e563cc29ecd53b084ec88ad5986e8b678171b
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-12-13 10:27:52 +0100

    ccs caroussel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* Menu */
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
@@ -13,3 +14,25 @@ if (toggle && nav) {
     page.classList.toggle("noscroll", isClosed);
   });
 }
+
+
+/* Caroussel */
+document.addEventListener('DOMContentLoaded', function() {
+  const beforeBtns = document.querySelectorAll('.before_btn');
+  const afterBtns = document.querySelectorAll('.after__btn');
+  const caroussels = document.querySelectorAll('.caroussel__program');
+
+  caroussels.forEach((caroussel, index) => {
+      let scrollAmount = 0;
+
+      afterBtns[index].addEventListener('click', () => {
+          caroussel.scrollBy({ left: caroussel.clientWidth, behavior: 'smooth' });
+          scrollAmount += caroussel.clientWidth;
+      });
+
+      beforeBtns[index].addEventListener('click', () => {
+          caroussel.scrollBy({ left: -caroussel.clientWidth, behavior: 'smooth' });
+          scrollAmount -= caroussel.clientWidth;
+      });
+  });
+});
